c8e8969e3515ed47804630d76441515587fd5ce1
stevewest <steve.west@veinteractive.com>
2013-01-08 15:59:59 +0000

    Adds migration for oAuth

---
+++ migrations/002_create_oauth_table.php
@@ -0,0 +1,41 @@
+<?php
+namespace Fuel\Migrations;
+
+class Create_ethanol_tables
+{
+
+	public function up()
+	{
+		echo('Creating table oauth');
+		\DBUtil::create_table('user_oauth',array(
+			'id'         => array(
+				'type'           => 'int',
+				'constraint'     => 11,
+				'null'           => 0,
+				'auto_increment' => true
+			),
+			'email'      => array(
+				'type'       => 'varchar',
+				'constraint' => 100,
+				'default'    => null
+			),
+			'driver'         => array(
+				'type'       => 'varchar',
+				'constraint' => 100,
+				'default'    => null
+			),
+			'user_id' => array(
+				'type'       => 'int',
+				'constraint' => 11,
+				'null'       => false
+			),
+		),array('id'));
+	}
+	
+	public function down()
+	{
+		echo('Dropping table oauth');
+		\DBUtil::drop_table('user_oauth');
+	}
+	
+}

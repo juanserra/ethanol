bae7bb6310f7e41bbe60f0059ac1e73afb3f537f
stevewest <uruwolf@gmail.com>
2013-03-01 16:37:51 +0000

    Adds full namespaces

--- bootstrap.php
+++ bootstrap.php
@@ -1,17 +1,17 @@
 <?php
 
 //Make sure orm is loaded first as we need this
-\Package::load('orm');
+\Fuel\Core\Package::load('orm');
 
 //For fuel v1 add to the finder path
 if(  class_exists('\Finder') )
 {
-	\Finder::instance()->add_path(__DIR__.DS.'..'.DS.'..');
+	\Fuel\Core\Finder::instance()->add_path(__DIR__.DS.'..'.DS.'..');
 }
 
-\Config::load('ethanol', true);
-\Config::load('ethanol_permissions', true);
-\Lang::load('ethanol', 'ethanol');
+\Fuel\Core\Config::load('ethanol', true);
+\Fuel\Core\Config::load('ethanol_permissions', true);
+\Fuel\Core\Lang::load('ethanol', 'ethanol');
 
 Ethanol\Auth::instance()->register_driver(array(
 	'database',

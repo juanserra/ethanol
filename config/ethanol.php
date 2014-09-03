20fccefce031cad528e3d7068e26dba8e87b1206
juanserra <juan.serra@veinteractive.com>
2014-09-03 13:13:18 +0100

    Guest user group removed from config

--- config/ethanol.php
+++ config/ethanol.php
@@ -1,18 +1,18 @@
 <?php
 return array(
-	
+
 	//Auth settings
 	//Default auth driver to use if none specified
 	'default_auth_driver' => 'database',
-	
+
 	//If log in attempts are logged or not. Disabling this will disable things
 	//like max log in attempts. It is recomended that this is enabled.
 	'log_log_in_attempts' => true,
 	'log_in_inital_delay' => 1, // start with a 1 second delay before being able to log in again
 	'log_in_delay_scale' => 2, // After a second failed attempt the delay will be 2 seconds, 4 for the next
 	'log_in_max_delay' => 45, // The maximum delay that will be applied between attempts
-	
-	
+
+
 	//Set to true to make users validate their email address before being able to log in
 	'activate_emails' => false,
 	'activation_key_length' => 10,
@@ -21,7 +21,7 @@ return array(
 	//The from address to be added to the activation email
 	'activation_email_from' => 'activation@mysite.com',
 	'activation_email_subject' => 'Welcome to the site!',
-	
+
 	//Various settings for the hashing class
 	'hashing' => array(
 		//Default hashing driver to use if none is specified.
@@ -32,7 +32,7 @@ return array(
 		//Default random driver to use if none is specified.
 		'default_driver' => 'Ethanol\Random_Driver_Simple',
 	),
-	
+
 	//Facebook driver settings
 	'facebook' => array(
 		'app_id' => '',
@@ -43,11 +43,11 @@ return array(
 		'client_id' => '',
 		'client_secret' => '',
 	),
-	
+
 	//Defines default guest groups
 	'guest' => array(
 		'groups' => array(
-			1, //Default id of the guest group
+			// Default id of the guest group (usually 1)
 		),
 	),
 );
